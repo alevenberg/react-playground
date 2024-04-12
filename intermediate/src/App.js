1bd54f45e1d2837090de7ebe8397bed2b4b32446
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-12 14:45:26 -0400

    impl(intermediate): show a list of names

--- intermediate/src/App.js
+++ intermediate/src/App.js
@@ -1,13 +1,37 @@
 
 // 1. Do a get request from this API and dump it on the screen
+// 2a. Create a flat array with the location data only
+// 2b. Create a table with the data in it (flatten location)
+// row is a person, columms: street name, street number, city, state ...
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const queryClient = new QueryClient()
 
 function App() {
+  const [people, SetPeople] = useState([])
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(`https://randomuser.me/api?results=20`);
+      const { results } = response.data;
+      return results;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData().then(data => {
+      SetPeople(data);
+    })
+  }, [])
+
   return (
     <>
       <h1>Hi</h1>
+      {people.map((person, idx) => <div key={idx}>{person.name.first}</div>)}
       < QueryClientProvider client={queryClient} >
         <Example />
       </QueryClientProvider >
@@ -28,6 +52,7 @@ function Example() {
 
   if (error) return 'An error has occurred: ' + error.message
 
+  console.log(data)
   return (
     <pre>{JSON.stringify(data, null, 2)}</pre>
   )
