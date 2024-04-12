0bb18ec2049518e859d084966e6ccd5bab108b27
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-11 20:50:31 -0400

    impl(my-app): get user data and present it on screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,28 @@
 import React, { useState, useRef, useEffect } from 'react'
+import axios from 'axios';
 
+// 1. Create a simple counter
+// 2. Fetch data from https://randomuser.me/api
+// 3. Dump data to string
 function App() {
   const [counter, setCounter] = useState(0);
+  const [randomUser, setRandomUser] = useState("");
+
+  async function getUser() {
+    try {
+      const response = await axios.get('https://randomuser.me/api');
+      setRandomUser(JSON.stringify(response))
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   function handleIncrement(e) {
     setCounter(counter + 1)
+  }
+
+  function handleGetUser(e) {
+    getUser()
   }
 
   return (
@@ -12,7 +30,9 @@ function App() {
       <h1> Counter</h1>
       <div> {counter} </div>
       <button onClick={handleIncrement}> Increment counter </button>
-
+      <h1> Get User</h1>
+      <button onClick={handleGetUser}> Get user </button>
+      <div> {randomUser} </div>
     </div>
   );
 }
