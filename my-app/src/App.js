555f8affa7cbc8b1a8ce1caec01a3929f6a4797e
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-11 20:21:56 -0400

    impl(my-app): add a simple counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,18 @@
+import React, { useState, useRef, useEffect } from 'react'
+
 function App() {
+  const [counter, setCounter] = useState([0])
+
+  function handleIncrement(e) {
+    setCounter(parseInt(counter) + 1)
+  }
+
   return (
     <div className="App">
+      <h1> Counter</h1>
+      <div> {counter} </div>
+      <button onClick={handleIncrement}> Increment counter </button>
+
     </div>
   );
 }
