6813917d20a777499466f4495d862b1d0b6b4aab
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-16 21:55:39 -0400

    impl(circle): initial redo

--- circle/src/App.js
+++ circle/src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 // https://www.youtube.com/watch?v=A0BmLYHLPZs
 function App() {
   const [points, setPoints] = useState([]);
+  const [undoPoints, setUndoPoints] = useState([]);
 
   function handlePlaceCircle(e) {
     const { clientX, clientY } = e;
@@ -13,7 +14,30 @@ function App() {
       y: clientY
     }])
   }
-  return (
+
+  function handleUndo() {
+    var newPoints = [...points];
+    var oldPoint = newPoints.pop();
+    setUndoPoints([
+      ...undoPoints,
+      oldPoint
+    ]);
+    setPoints(newPoints);
+  }
+
+  function handleRedo() {
+    var undoPointsArr = [...undoPoints];
+    var oldPoint = undoPointsArr.pop();
+    setUndoPoints(undoPointsArr);
+    setPoints([
+      ...points,
+      oldPoint
+    ])
+  }
+
+  return (<>
+    <button onClick={handleUndo}>Undo </button>
+    <button onClick={handleRedo}>Redo </button>
     <div className="App" onClick={handlePlaceCircle}>
       <h1> Circle</h1>
       {points.map(point => (<div className="Point" style={{
@@ -21,7 +45,7 @@ function App() {
         top: point.y + "px"
       }}></div>))
       }
-    </div >
+    </div ></>
   );
 }
 
