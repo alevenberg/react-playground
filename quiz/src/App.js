360702a96daeafc724406f7b2d7ff8c0107f6b5c
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-17 12:01:05 -0400

    impl(quiz): show incorrect/correct once submitted

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-
+// Display a quiz and click through
 const question = {
   prompt: "What color is the sky?",
-  correctAnswer: "blue",
+  correctAnswer: "Blue",
   answers: [
     "Red",
     "Blue",
@@ -16,11 +16,34 @@ const question = {
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [answer, setAnswer] = useState();
+  const [result, setResult] = useState();
+
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (answer === question.correctAnswer) {
+      setResult("Correct");
+    } else {
+      setResult("Incorrect");
+    }
+  }
 
   return (
     <div className="App">
       <h1> Quiz</h1>
       <h2>{question.prompt}</h2>
+      <form onSubmit={handleSubmit}>
+        <select value={answer} onChange={handleChange}>
+          {question.answers.map((a) => (<option value={a}> {a}</option>))}
+        </select>
+        <input type="submit" />
+      </form>
+      <div>{result}</div>
     </div>
   );
 }
