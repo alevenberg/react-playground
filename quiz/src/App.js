84dc9744ff407824005cf7b21bf20dc9862a3489
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-16 22:18:33 -0400

    impl(quiz): initial prompt

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+const question = {
+  prompt: "What color is the sky?",
+  correctAnswer: "blue",
+  answers: [
+    "Red",
+    "Blue",
+    "Green",
+    "Yellow"
+  ]
+};
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
   return (
     <div className="App">
+      <h1> Quiz</h1>
+      <h2>{question.prompt}</h2>
     </div>
   );
 }
