e506d5d4eee6d4238780055c5d71a327be154203
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-21 22:15:47 -0400

    impl(jobboard): try to get jobs

--- jobboard/src/App.js
+++ jobboard/src/App.js
@@ -8,7 +8,19 @@ import axios from 'axios';
 // https://hacker-news.firebaseio.com/v0/jobstories.json
 // 2.  https://hacker-news.firebaseio.com/v0/item/35908337.json:
 function App() {
-  const [jobIds, SetJobIds] = useState()
+  const [jobIds, SetJobIds] = useState([])
+  const [jobs, SetJobs] = useState([])
+  const [index, SetIndex] = useState(6)
+
+  async function fetchJob(jobId) {
+    try {
+      const response = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${jobId}.json`);
+      const results = response.data;
+      return results;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   async function fetchJobIds() {
     try {
@@ -20,11 +32,28 @@ function App() {
     }
   }
 
+  function handleLoadMore(e) {
+    e.preventDefault();
+    var newIndex = index + 6;
+    if (newIndex >= jobIds.length) {
+      newIndex = jobIds.length;
+    }
+    SetIndex(newIndex);
+    console.log(newIndex);
+  }
+
   useEffect(() => {
     fetchJobIds().then(data => {
       console.log(data);
       SetJobIds(data);
     })
+    for (let i = 0; i < jobIds.length; i++) {
+      fetchJob(jobIds[i]).then(data => {
+        console.log(data);
+        const newJobs = [...jobs, data]
+        SetJobs(newJobs);
+      })
+    }
   }, [])
 
   return (
@@ -33,6 +62,10 @@ function App() {
       <div>
         {JSON.stringify(jobIds)}
       </div>
+      <div>
+        {JSON.stringify(jobs)}
+      </div>
+      <button onClick={handleLoadMore}>Load more jobs</button>
     </div>
   );
 }
