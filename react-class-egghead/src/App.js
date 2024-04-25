c62d5438bf73fd2cf77880c581417358d6e2c5fd
Anna Levenberg <annarose.levenberg@gmail.com>
2024-04-24 22:45:43 -0400

    impl(react-class-egghead): add intial component

--- react-class-egghead/src/App.js
+++ react-class-egghead/src/App.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+// Stateless
 const App = () => <h1> Hello </h1>
 
-export default App
+// Stateful
+class App2 extends React.Component {
+  render() {
+    return <h1> Hello2 </h1>
+  }
+}
+
+// Multiple divs
+class App3 extends React.Component {
+  render() {
+    return (<><h1> Hello3 </h1> <h2> Hey</h2></>)
+  }
+}
+
+class App4 extends React.Component {
+  render() {
+    let txt = this.props.txt;
+    return (<><h1> {this.props.txt} </h1> <h2> {txt}</h2></>)
+  }
+}
+
+export default App4
--- react-class-egghead/src/index.js
+++ react-class-egghead/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App4 from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App4 txt="this is the prop test" />
   </React.StrictMode>
 );
